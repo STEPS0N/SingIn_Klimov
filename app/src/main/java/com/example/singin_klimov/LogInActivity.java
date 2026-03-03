b3d8167d3f1c293cc0dd49813655231b3a6d7fd6
STEPS0N <stepan.klimoff@mail.ru>
2026-03-03 22:09:50 +0500

    Программирование класса LogInActivity. Климов

--- app/src/main/java/com/example/singin_klimov/LogInActivity.java
+++ app/src/main/java/com/example/singin_klimov/LogInActivity.java
@@ -1,6 +1,10 @@
 package com.example.singin_klimov;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,36 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
+
+        TextView btnOpenSingIn = findViewById(R.id.btn_open_sign_in);
+
+        btnOpenSingIn.setOnClickListener(v -> {
+            Intent SingIn = new Intent(this, RegInActivity.class);
+            startActivity(SingIn);
+        });
+
+        Button btnLogIn = findViewById(R.id.btn_log_in);
+
+        btnLogIn.setOnClickListener(v -> {
+            TextView etEmail = findViewById(R.id.et_email);
+            TextView etPassword = findViewById(R.id.et_password);
+
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            if (email.isEmpty()){
+                Toast.makeText(this, "Не указана почта пользователя", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (password.isEmpty()){
+                Toast.makeText(this, "Не указан пароль пользователя", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(this, "Пользователь авторизован", Toast.LENGTH_SHORT).show();
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
