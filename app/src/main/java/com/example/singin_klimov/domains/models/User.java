874722bd15b1b6a72d006997453673d12bc53ac2
YakovSm <vollaston@gmail.com>
2026-03-06 14:01:48 +0500

    Разработанный класс авторизации. Климов

---
+++ app/src/main/java/com/example/singin_klimov/domains/models/User.java
@@ -0,0 +1,14 @@
+package com.example.singin_klimov.domains.models;
+
+public class User {
+
+    public Integer id;
+    public String email;
+    public String password;
+    public String firstname;
+    public String lastname;
+    public String surename;
+    public String dateOfBirth;
+    public Integer gender;
+    public String token;
+}
